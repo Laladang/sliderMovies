d170b884eb1809608b5b6f913c9c3c82fd07870d
Ladang <Ladang@users.noreply.github.com>
2017-07-15 23:01:49 +0200

    first commit

---
+++ js/functions.js
@@ -0,0 +1,110 @@
+$(document).ready(function() {
+
+	// Get movies informations
+	fetch('https://api.themoviedb.org/3/movie/upcoming?api_key=e082a5c50ed38ae74299db1d0eb822fe').then(res => res.json()).then(data => {
+	const movie = data.results.map(e => ({title: e.title, image: e.backdrop_path, note: e.vote_average, synopsis: e.overview}));
+
+	/*  VARIABLES
+		========= */
+
+	var i = 0;
+	var mLenght = movie.length;
+	
+	/* SLIDER MOVIE
+		============ */
+
+	movie.forEach(_d => {
+
+		/* Create div */
+		const movie = $('<div>', {class: 'movie'})
+		const movieDescription = $('<div>', {class: 'movie-description'})
+
+		/* Image */
+		if(_d.image == null){
+			_d.image = 'images/img-default.jpg';
+		} else {
+			_d.image = `https://image.tmdb.org/t/p/w650/${_d.image}`;
+		}
+
+		const eImg = $('<img>', {class: 'movie-img', src: _d.image})[i]
+		movie.append(eImg);
+
+		/* Title */
+		const eTitle = $('<h2>', {class: 'movie-title'}).text(_d.title)[0]
+		movieDescription.append(eTitle)
+
+		/* Note */
+		var note = Math.round(_d.note)
+		const eNote = $('<p>', {class: 'movie-note'})
+		n = 0;
+		while (n < note){
+			const eNoteImg = $('<img>', {src: 'images/star.png'})[i]
+			eNote.append(eNoteImg);
+			n++;
+		}
+		movieDescription.append(eNote)
+
+		/* Synopsis */
+		const eSynopsis = $('<p>', {class: 'movie-synopsis'}).text(_d.synopsis.substring(0,200) + '...')[0]
+		movieDescription.append(eSynopsis)
+
+		/* Bullet */
+		const bullet = $('<li>')
+
+		/* Show content in div */
+		movie.append(movieDescription);
+		$('.slider-movie').append(movie);
+		$('.slider-bullet').append(bullet);
+
+	})
+
+
+	/*  SLIDER NAVIGATION
+		=================	*/
+
+	$('.movie').eq(0).addClass('visible');
+	$('.slider-bullet li').eq(0).addClass('active');
+
+	// Previous movie
+	$('a.movie-previous').click(function() {
+		$('.movie').eq(i).removeClass('visible');
+		$('.slider-bullet li').eq(i).removeClass('active');
+		
+		if((i % mLenght) == 0){
+			i = mLenght-1;
+		} else {
+			i--			
+		}
+		$('.movie').eq(i).addClass('visible');
+		$('.slider-bullet li').eq(i).addClass('active');
+	});
+
+	// Next movie
+	$('a.movie-next').click(function() {
+		$('.movie').eq(i).removeClass('visible');
+		$('.slider-bullet li').eq(i).removeClass('active');
+
+		if((i % mLenght) == (mLenght - 1)){
+			i = 0;	
+		} else {
+			i++
+		}
+		$('.movie').eq(i).addClass('visible');
+		$('.slider-bullet li').eq(i).addClass('active');
+	});
+
+	// Bullet navigation
+	$('.slider-bullet li').click(function() {
+		var iMovie = $(this).index();
+		$('.movie').eq(i).removeClass('visible');
+		$('.movie').eq(iMovie).addClass('visible');
+		$('.slider-bullet li').eq(i).removeClass('active');
+		$('.slider-bullet li').eq(iMovie).addClass('active');
+
+		i = iMovie;
+	})
+
+
+})
+
+});
